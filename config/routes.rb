01b5d327bfa21c275f6ebd24947829df3dd3c349
cbestiva <cbestiva@gmail.com>
2014-03-20 14:22:15 -0700

    added bootsrap css and js files

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 TreasureboxApp::Application.routes.draw do
   devise_for :users
-  devise_scope :user do 
-    root to: "devise/sessions#new"
-  end
+  # devise_scope :user do 
+  #   root to: "devise/sessions#new"
+  # end
 
-  get "sites/index"
+  root to: "sites#index"
 end
