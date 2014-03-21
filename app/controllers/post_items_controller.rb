95bc1159169c1c8f55d8eef563cd6ec6d6bc84a3
cbestiva <cbestiva@gmail.com>
2014-03-21 11:33:15 -0700

    added create method for postitems

--- app/controllers/post_items_controller.rb
+++ app/controllers/post_items_controller.rb
@@ -1,2 +1,11 @@
 class PostItemsController < ApplicationController
+  def create
+    new_post_item = params.require(:post_item).permit(:name, :category, :description, :price)
+    post_item = PostItem.create(new_post_item)
+
+    respond_to do |f|
+      f.html {redirect_to profile_path}
+      f.json {render json: post_item}
+    end
+  end
 end
