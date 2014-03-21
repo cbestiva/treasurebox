3e9d62f5235ec733906f64fb8901f0e062874929
cbestiva <cbestiva@gmail.com>
2014-03-21 09:20:54 -0700

    changed redirect after user signs inin application controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 TreasureboxApp::Application.routes.draw do
   devise_for :users
-  # devise_scope :user do 
-  #   root to: "devise/sessions#new"
-  # end
+  
   root to: "sites#index"
   get "users/show/:id" => "users#show", as: "profile"
 end
